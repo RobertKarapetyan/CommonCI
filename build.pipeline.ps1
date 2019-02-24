5522b64a21f447be401146ae5070a3aa00890bcd
engineer0001 <engineernumber001@gmail.com>
2019-02-24 11:49:55 -0800

    Add Test Task

--- build.pipeline.ps1
+++ build.pipeline.ps1
@@ -4,13 +4,9 @@ Include "./build.task.restore.ps1"
 
 Include "./build.task.rebuild.ps1"
 
-#Include "./build.task.test.ps1"
-
-#Include "./build.task.testCoverage.ps1"
+Include "./build.task.test.ps1"
 
 Task -name BuildPipeLine `
-    -depends Init, Restore, Rebuild
-	
-#, Restore, Rebuild, Test, TestCoverage `
+    -depends Init, Restore, Rebuild, Test
 
 Task -name default -depends BuildPipeLine
---
+++ build.task.test.ps1
@@ -0,0 +1,16 @@
+﻿Include ".\build.config.ps1"
+
+Task -name Test -action {
+    Write-Host ""
+    TestDlls
+    Write-Host ""
+}
+
+Function TestDlls
+{
+    Write-Host "`tConsoleApp.Tests.dll:"
+    Write-Host ""
+    $cmd = "&'$vsTest' .\.build\ConsoleApp.Tests.dll /logger:trx"
+    Invoke-Expression $cmd
+    Write-Host ""
+}
