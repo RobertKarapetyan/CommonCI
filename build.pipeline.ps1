37366e9a5b07092156f5d7c2fe97026718b98e10
engineer0001 <engineernumber001@gmail.com>
2019-02-24 11:35:16 -0800

    Add project files.

---
+++ build.pipeline.ps1
@@ -0,0 +1,16 @@
+﻿Include "./build.task.init.ps1"
+
+Include "./build.task.restore.ps1"
+
+#Include "./build.task.rebuild.ps1"
+
+#Include "./build.task.test.ps1"
+
+#Include "./build.task.testCoverage.ps1"
+
+Task -name BuildPipeLine `
+    -depends Init, Restore
+	
+#, Restore, Rebuild, Test, TestCoverage `
+
+Task -name default -depends BuildPipeLine
